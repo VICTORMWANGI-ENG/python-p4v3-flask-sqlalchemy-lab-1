c8540aac1e369d309bc4ae5c69a46d8a368a8e25
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-10-09 15:31:52 +0300

    feat: Add earthquake API endpoints
    Adds API endpoints for retrieving all earthquakes, earthquakes by ID, and earthquakes with a magnitude greater than or equal to a specified value.
    
    Includes a model for earthquakes, which stores their magnitude, location, and year.
    
    The API endpoints return JSON responses.

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response,jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,8 +20,48 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+
+@app.route('/earthquakes')
+def earthquakes():
+    quakes = Earthquake.query.all()
+    response=[{
+        'id': eq.id,
+        'magnitude': eq.magnitude,
+        'location': eq.location,
+        'year': eq.year
+    }for eq in quakes]
+    return make_response(jsonify(response), 200) 
+
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    eq = Earthquake.query.get(id)
+    if eq is None:
+        return make_response({
+            'message': f'Earthquake {id} not found.'
+        }, 404)
+    return make_response({
+        'id': eq.id,
+        'magnitude': eq.magnitude,
+        'location': eq.location,
+        'year': eq.year
+        }, 200)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')  
+def earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()  
+    quake_list = [{
+        'id': eq.id,
+        'location': eq.location,
+        'magnitude': eq.magnitude,
+        'year': eq.year
+    } for eq in quakes]
+
+    return make_response({
+        'count': len(quake_list),
+        'quakes': quake_list
+    }, 200)
 
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5000, debug=True)
